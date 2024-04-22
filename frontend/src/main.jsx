054f3aa8d9930005cd533727c7078987c4560cd2
jiangxin1 <jiang.xin1@northeastern.edu>
2024-04-22 01:02:01 -0700

    remove test

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
-import TestComponent from './test.jsx'; // 假设 test.jsx 中的组件名为 TestComponent
 
 ReactDOM.render(
   <React.StrictMode>
-    <TestComponent />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
